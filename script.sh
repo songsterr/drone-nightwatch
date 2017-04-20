795b74e3a4fc53a0cba01947894401ceb99a4973
Aleksei Krasnoperov <aleksei@krasnoperov.me>
2017-04-20 23:52:13 +0700

    Remove limits

--- script.sh
+++ script.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-PERSON=random nightwatch --reporter=/test/nightwatch.reporter.js --env ci --test tests/*.js || true
+PERSON=random nightwatch --reporter=/test/nightwatch.reporter.js --env ci || true
